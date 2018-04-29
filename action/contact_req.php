4c782b08121bb973f658a01c230d7f59455aee10
Yogendra <yogendra.danwar@gmail.com>
2018-04-29 20:55:06 +0530

    made changes in the mail functionality

--- action/contact_req.php
+++ action/contact_req.php
@@ -8,20 +8,23 @@ if( !empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['phone'])
 	
 	$to = "info@costadvantage.net, $email";
 	$subject = 'Contact US';
-	$message = "Hello Your message is received by us: <br> Name: $name <br>
-	<br> Email: $email <br>
-	<br> Phone: $phone <br>
-	<br> Message: $message <br>
-	You may expect a reply from us withing 24-48  hrs.<br><br>
-	Thank You<br>
-";
+	$message = "Hello Your message is received by us:" . 
+	"\r\n" ."Name: $name " . 
+	"\r\n" ."Email: $email " .
+	"\r\n" ."Phone: $phone " .
+	"\r\n" ."Message: $message " .
+	"\r\n" ."You may expect a reply from us withing 24-48  hrs." .
+	"\r\n" . "Thank You";
 $headers = 'From: info@costadvantage.net' . "\r\n" .
 	'Reply-To: info@costadvantage.net' . "\r\n" .
 	'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $message, $headers);
+$mail_log = mail($to, $subject, $message, $headers);
 
-header('location:../contactus.php?success');
+echo $message;
+die('1122');
+
+//header('location:../contactus.php?success');
 
 } else {
 	error_log('nai chal raho');
