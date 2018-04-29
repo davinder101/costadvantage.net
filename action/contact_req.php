25bd1a697400ecc0554159ec0d43ecc4d61ff902
DAVINDER BHASIN <Davinder@DAVINDERs-MacBook-Pro.local>
2018-04-29 19:56:44 +0530

    costadvantage version 2.2

--- action/contact_req.php
+++ action/contact_req.php
@@ -8,15 +8,15 @@ if( !empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['phone'])
 	
 	$to = "info@costadvantage.net, $email";
 	$subject = 'Contact US';
-	$message = "Hello Your message is received by us: <br/> Name: $name <br/>
-	<br/> Email: $email <br/>
-	<br/> Phone: $phone <br/>
-	<br/> Message: $message <br/>
-	You may expect a reply from us withing 24-48  hrs.<br/><br/>
-	Thank You<br/>
+	$message = "Hello Your message is received by us: <br> Name: $name <br>
+	<br> Email: $email <br>
+	<br> Phone: $phone <br>
+	<br> Message: $message <br>
+	You may expect a reply from us withing 24-48  hrs.<br><br>
+	Thank You<br>
 ";
-$headers = 'From: contact@ecostadvantage.com' . "\r\n" .
-	'Reply-To: contact@ecostadvantage.com' . "\r\n" .
+$headers = 'From: info@costadvantage.net' . "\r\n" .
+	'Reply-To: info@costadvantage.net' . "\r\n" .
 	'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
